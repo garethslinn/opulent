0e56f89f7f3c1441b607a76bd42e97a308cebd09
garethslinn <gslinn@gmail.com>
2024-07-22 09:59:34 +0100

    apply skills stubs

---
+++ src/app/stubs/industries.ts
@@ -0,0 +1,38 @@
+export const industries = [
+    {
+        industry: "Comparison Sites",
+        companies: ["Uswitch"]
+    },
+    {
+        industry: "Construction",
+        companies: ["Logica"]
+    },
+    {
+        industry: "Digital Agencies",
+        companies: ["AKQA", "Draft", "Emotive", "MRM", "Salmon", "Shout"]
+    },
+    {
+        industry: "Finance",
+        companies: ["AIG", "Grant Thornton"]
+    },
+    {
+        industry: "Gambling",
+        companies: ["Genius Bet", "William Hill"]
+    },
+    {
+        industry: "Oil/Gas",
+        companies: ["BP"]
+    },
+    {
+        industry: "Retail",
+        companies: ["Argos", "Essentra", "NBrown", "Premier Foods", "Sainsburys", "Tesco"]
+    },
+    {
+        industry: "Sport",
+        companies: ["Dugout"]
+    },
+    {
+        industry: "Telecommunications",
+        companies: ["BT", "Cisco"]
+    }
+];
---
+++ src/app/stubs/techskills.ts
@@ -0,0 +1,156 @@
+export const techSkills = [
+    {
+        title: "JavaScript",
+        type: 1, // 1 = core
+        image: "/assets/images/skill/javascript.svg",
+        year_started: "2010",
+        fixed_years: null
+    },
+    {
+        title: "TypeScript",
+        type: 1,
+        image: "/assets/images/skill/typescript.svg",
+        year_started: "2018",
+        fixed_years: null
+    },
+    {
+        title: "ReactJS",
+        type: 1,
+        image: "/assets/images/skill/reactjs.svg",
+        year_started: "2016",
+        fixed_years: null
+    },
+    {
+        title: "Redux",
+        type: 1,
+        image: "/assets/images/skill/redux.svg",
+        year_started: "2018",
+        fixed_years: null
+    },
+    {
+        title: "NodeJS",
+        type: 1,
+        image: "/assets/images/skill/nodejs.svg",
+        year_started: "2015",
+        fixed_years: null
+    },
+    {
+        title: "Jest",
+        type: 1,
+        image: "/assets/images/skill/jest.svg",
+        year_started: "2015",
+        fixed_years: null
+    },
+    {
+        title: "Cypress",
+        type: 2,
+        image: "/assets/images/skill/cypress.svg",
+        year_started: null,
+        fixed_years: 5
+    },
+    {
+        title: "CSS",
+        type: 1,
+        image: "/assets/images/skill/css.svg",
+        year_started: "2004",
+        fixed_years: null
+    },
+    {
+        title: "SCSS",
+        type: 1,
+        image: "/assets/images/skill/scss.svg",
+        year_started: "2015",
+        fixed_years: null
+    },
+    {
+        title: "Styled Components",
+        type: 1,
+        image: "/assets/images/skill/styled_components.svg",
+        year_started: "2015",
+        fixed_years: null
+    },
+    {
+        title: "Bootstrap",
+        type: 1,
+        image: "/assets/images/skill/bootstrap.svg",
+        year_started: "2010",
+        fixed_years: null
+    },
+    {
+        title: "SonarQube",
+        type: 2,
+        image: "/assets/images/skill/sonarqube.svg",
+        year_started: null,
+        fixed_years: 1
+    },
+    {
+        title: "NightWatch",
+        type: 2,
+        image: "/assets/images/skill/nightwatch.svg",
+        year_started: null,
+        fixed_years: 1
+    },
+    {
+        title: "AWS",
+        type: 1,
+        image: "/assets/images/skill/aws.svg",
+        year_started: "2018",
+        fixed_years: null
+    },
+    {
+        title: "Azure",
+        type: 1,
+        image: "/assets/images/skill/azure.svg",
+        year_started: null,
+        fixed_years: 3
+    },
+    {
+        title: "Jenkins",
+        type: 2,
+        image: "/assets/images/skill/jenkins.svg",
+        year_started: null,
+        fixed_years: 3
+    },
+    {
+        title: "Webpack",
+        type: 1,
+        image: "/assets/images/skill/webpack.svg",
+        year_started: null,
+        fixed_years: 5
+    },
+    {
+        title: "Responsive Web",
+        type: 1,
+        image: "/assets/images/skill/webpack.svg",
+        year_started: "2012",
+        fixed_years: null
+    },
+    {
+        title: "GIT",
+        type: 1,
+        image: "/assets/images/skill/git.svg",
+        year_started: "2011",
+        fixed_years: null
+    },
+    {
+        title: "Express",
+        type: 1,
+        image: "/assets/images/skill/express.svg",
+        year_started: null,
+        fixed_years: 5
+    },
+    {
+        title: "MongoDB",
+        type: 1,
+        image: "/assets/images/skill/mongodb.svg",
+        year_started: null,
+        fixed_years: 3
+    },
+    {
+        title: "Vector Graphics",
+        type: 3,
+        image: "/assets/images/skill/vector.svg",
+        year_started: null,
+        fixed_years: "1990"
+    },
+]
