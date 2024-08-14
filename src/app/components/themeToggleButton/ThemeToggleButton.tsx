b92b40f5d0bf2feb3b5fea15cb789f9f905d9f69
garethslinn <gslinn@gmail.com>
2024-08-14 22:43:31 +0100

    fix accessibility issue

--- src/app/components/themeToggleButton/ThemeToggleButton.tsx
+++ src/app/components/themeToggleButton/ThemeToggleButton.tsx
@@ -50,6 +50,7 @@ const ThemeToggleButton: React.FC<ThemeToggleButtonProps> = ({
                     strokeLinejoin="round"
                     className="feather feather-sun"
                 >
+                    <title>sun icon depicting light mode</title>
                     <circle cx="12" cy="12" r="5"></circle>
                     <line x1="12" y1="1" x2="12" y2="3"></line>
                     <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -73,6 +74,7 @@ const ThemeToggleButton: React.FC<ThemeToggleButtonProps> = ({
                     strokeLinejoin="round"
                     className="feather feather-moon"
                 >
+                    <title>moon icon depicting dark mode</title>
                     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
                 </svg>
             )}
